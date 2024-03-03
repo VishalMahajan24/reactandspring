0c18959bd0ae0b7d2df8eef0b0d0348613cf88b4
VishalMahajan24 <113411277+VishalMahajan24@users.noreply.github.com>
2024-03-04 01:19:21 +0530

    new dock

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM maven:3.8.3-openjdk-17 AS build 
+COPY . .
+
+RUN mvn clean package -DskipTests
+COPY --from=build /target/bootReact-0.0.1-SNAPSHOT.jar render.jar
+EXPOSE 8080
+ENTRYPOINT [ "java","-jar","bootReact.jar" ]  
--- src/main/java/com/demo/bootReact/BootReactApplication.java
+++ src/main/java/com/demo/bootReact/BootReactApplication.java
@@ -2,8 +2,10 @@ package com.demo.bootReact;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+
 public class BootReactApplication {
 
 	public static void main(String[] args) {
